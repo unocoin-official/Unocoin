5fe17794ee72d869b7f6f5b529b26b3ef1473f33
Antoine De Vuyst <fortyonepointsix@gmail.com>
2021-04-27 01:14:27 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,51 @@
 require("@nomiclabs/hardhat-waffle");
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
- defaultNetwork: "mainnet",
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
- networks: {
-   hardhat: {
-   },
-   mainnet: {
-     url: "",
-     accounts: [""]
-   },
-   rinkeby: {
-     url: "",
-     accounts: [""]
-   }
- },
+module.exports = {
+  defaultNetwork: "RSK Test",
 
-solidity: {
-  version: "0.8.3"
-},
+  networks: {
+    hardhat: {
+    },
+    rsk: {
+      url: "https://public-node.testnet.rsk.co",
+      accounts: [""]
+    },
 
-paths: {
-  sources: "./contracts",
-  tests: "./test",
-  cache: "./cache",
-  artifacts: "./artifacts"
-},
+  rinkeby: {
+        url: "",
+        accounts: [""]
+    }
+  },
 
-mocha: {
-  timeout: 20000
-}
+  solidity: {
+    version: "0.8.3"
+  },
+
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+
+  mocha: {
+    timeout: 20000
+  }
+};
